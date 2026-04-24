4843a12f58d528e63679ff6f900ace28509ac081
Connor Buckley <connorbuckley144@gmail.com>
2026-04-23 22:37:35 -0700

    test: add unit tests for core blackjack model class logic

---
+++ src/test/java/edu/sjsu/cs151/model/DealerTest.java
@@ -0,0 +1,32 @@
+package edu.sjsu.cs151.model;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DealerTest {
+    private Dealer dealer;
+
+    @BeforeEach
+    public void setup() {
+        dealer = new Dealer();
+    }
+
+    @Test
+    public void shouldHit16() {
+        dealer.addCard(new Card(Suit.CLUBS, Rank.QUEEN));
+        dealer.addCard(new Card(Suit.CLUBS, Rank.SIX));
+
+        assertTrue(dealer.shouldHit());
+    }
+
+    @Test
+    public void shouldHit17() {
+        dealer.addCard(new Card(Suit.CLUBS, Rank.QUEEN));
+        dealer.addCard(new Card(Suit.CLUBS, Rank.SEVEN));
+
+        assertFalse(dealer.shouldHit());
+    }
+
+}
---
+++ src/test/java/edu/sjsu/cs151/model/DeckTest.java
@@ -0,0 +1,27 @@
+package edu.sjsu.cs151.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+    private Deck deck;
+
+    @BeforeEach
+    public void setup() {
+        deck = new Deck();
+    }
+
+    @Test
+    public void isEmptyDeck() {
+        for (int i = 0; i < 52; i++) {
+            deck.dealCard();
+        }
+
+        assertEquals(0, deck.size());
+        assertTrue(deck.isEmpty());
+    }
+
+}

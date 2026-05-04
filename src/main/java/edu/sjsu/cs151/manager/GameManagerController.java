27f3e17e1a49612db63343f5842ac1e74d10ce75
Chelsea Pham <chelsea21404@gmail.com>
2026-05-03 20:34:49 -0700

    Added login and account creation methods along with some skeleton methods

--- src/main/java/edu/sjsu/cs151/manager/GameManagerController.java
+++ src/main/java/edu/sjsu/cs151/manager/GameManagerController.java
@@ -1,6 +1,8 @@
 package edu.sjsu.cs151.manager;
 
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GameManagerController {
@@ -16,6 +18,48 @@ public class GameManagerController {
         accountManager = new UserAccountManager();
         scoreManager = new HighScoreManager();
     }
-    
+
+    public void initialize() {
+        accountManager.loadAccounts();
+        scoreManager.loadScores();
+        showLoginScreen();
+    }
+
+    public boolean login(String username, String password) {
+        UserAccount account = accountManager.login(username, password);
+        if (account != null) {
+            currentUser = account;
+            showMainMenu();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean createAccount(String username, String password) {
+        boolean created = accountManager.createAccount(username, password);
+        if (created) {
+            scoreManager.initialDefaultScore(username);
+            currentUser = accountManager.getAccount(username);
+            showMainMenu();
+            return true;
+        }
+        return false;
+    }
+
+    private void showLoginScreen() {
+
+    }
+
+    private VBox buildLoginScreen() {
+        return new VBox();
+    }
+
+    public void showMainMenu() {
+
+    }
+
+    private HBox buildMainMenu() {
+        return new HBox();
+    }
+
 }
-   

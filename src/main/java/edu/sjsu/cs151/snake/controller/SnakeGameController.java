f5ca0ca1d172319f669f50482406f446b32a3b09
ShannonYuHsinLo <yuhsin.lo@sjsu.edu>
2026-05-05 01:22:24 -0700

    fix: handleKeyPress

--- src/main/java/edu/sjsu/cs151/snake/controller/SnakeGameController.java
+++ src/main/java/edu/sjsu/cs151/snake/controller/SnakeGameController.java
@@ -52,7 +52,7 @@ public class SnakeGameController {
 
             case ESCAPE -> model.togglePause();
 
-            default    -> {}            
+            default -> {}            
 
         }
     }

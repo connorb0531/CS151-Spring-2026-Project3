072c1e19dda3ed6dcc00a037ae32f5bdef492fe2
ShannonYuHsinLo <yuhsin.lo@sjsu.edu>
2026-05-04 20:26:26 -0700

    feat: Point.java, main's GameManagerController, enum Direction

---
+++ src/main/java/edu/sjsu/cs151/manager/Main.java
@@ -0,0 +1,17 @@
+package edu.sjsu.cs151.manager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+        GameManagerController controller = new GameManagerController(primaryStage);
+        controller.initialize(); //calling showLoginScreen()
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
--- src/main/java/edu/sjsu/cs151/snake/model/Direction.java
+++ src/main/java/edu/sjsu/cs151/snake/model/Direction.java
@@ -1,3 +1,22 @@
-public enum Direction {
+package edu.sjsu.cs151.snake.model;
 
+public enum Direction {
+    UP(0, -1),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    RIGHT(1, 0);
+
+
+    public final int dx;
+    public final int dy;
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public boolean isOpposite(Direction other) {
+        return this.dx == -other.dx && this.dy == -other.dy;
+    }
+   
 }
